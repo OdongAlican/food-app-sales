e04d26c19056233a55aac52264a23d9a5b147ea3
Sunday Odong <sandieo.2020@gmail.com>
2023-06-07 03:29:46 +0300

    initial commit

---
+++ src/core/routes/AppRoutes.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router";
+import { ROUTES } from "./routes";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+     
+    </Routes>
+  )
+}
+export default AppRoutes;
